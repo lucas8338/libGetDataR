1906ae38a4b91bbc10404c41a60578cc9d0d8c20
lucas <lucas.ma8338@gmail.com>
2022-07-28 11:50:55 -0300

    added progress to split

--- R/split.R
+++ R/split.R
@@ -1,18 +1,15 @@
 #' split a timeseries data (data.frame) in to a x/y to fit a model
 
-library(progress)
-
-split.x_y_timeseries_split<- function(data,input_size,output_size){
+split.x_y_timeseries_split<- function(data,input_size,output_size,verbose=TRUE){
   sequence_size<- input_size+output_size
   if (nrow(data)%%sequence_size!=0){
     data<-data[((nrow(data)%%sequence_size)+1):nrow(data),]
   }
   data.x<-list()
   data.y<-list()
-  iters<- 1:(nrow(data)-sequence_size)+1
-  verb<- progress::progress_bar$new(total=length(iters))
+  iters<<- 1:((nrow(data)-sequence_size)+1)
   for (i in iters){
-    iters$tick()
+    if (verbose==TRUE){svMisc::progress(i,length(iters))}
     visible.data<-data[i:(sequence_size+(i-1)),]
     data.x<-append(data.x,list(as.data.frame(visible.data[1:input_size,])))
     data.y<-append(data.y,list(as.data.frame(visible.data[(input_size+1):nrow(visible.data),])))
