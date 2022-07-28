94f46497a20bb3eb0cf572a13cd094f983ba8d22
lucas <lucas.ma8338@gmail.com>
2022-07-28 11:56:37 -0300

    removed the condition to verbose to increase speed

--- R/split.R
+++ R/split.R
@@ -1,6 +1,6 @@
 #' split a timeseries data (data.frame) in to a x/y to fit a model
 
-split.x_y_timeseries_split<- function(data,input_size,output_size,verbose=TRUE){
+split.x_y_timeseries_split<- function(data,input_size,output_size){
   sequence_size<- input_size+output_size
   if (nrow(data)%%sequence_size!=0){
     data<-data[((nrow(data)%%sequence_size)+1):nrow(data),]
@@ -9,7 +9,7 @@ split.x_y_timeseries_split<- function(data,input_size,output_size,verbose=TRUE){
   data.y<-list()
   iters<<- 1:((nrow(data)-sequence_size)+1)
   for (i in iters){
-    if (verbose==TRUE){svMisc::progress(i,length(iters))}
+    svMisc::progress(i,length(iters))
     visible.data<-data[i:(sequence_size+(i-1)),]
     data.x<-append(data.x,list(as.data.frame(visible.data[1:input_size,])))
     data.y<-append(data.y,list(as.data.frame(visible.data[(input_size+1):nrow(visible.data),])))
