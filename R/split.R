2f2094266f2d3fa25b1a309b9fda473e0f951dd3
lucas <lucas.ma8338@gmail.com>
2022-07-31 21:34:40 -0300

    added parallelization to split

--- R/split.R
+++ R/split.R
@@ -1,19 +1,24 @@
 #' split a timeseries data (data.frame) in to a x/y to fit a model
 
-split.x_y_timeseries_split<- function(data,input_size,output_size){
+split.x_y_timeseries_split<- function(data,input_size,output_size,n_jobs=-1){
+  if (n_jobs>0){
+    cl<- parallel::makeCluster(n_jobs)
+    doParallel::registerDoParallel(cl)
+  }
   sequence_size<- input_size+output_size
   if (nrow(data)%%sequence_size!=0){
     data<-data[((nrow(data)%%sequence_size)+1):nrow(data),]
   }
   data.x<-list()
   data.y<-list()
-  iters<<- 1:((nrow(data)-sequence_size)+1)
-  for (i in iters){
+  iters<- 1:((nrow(data)-sequence_size)+1)
+  doParallel::foreach(i = iters)%dopar%{
     svMisc::progress(i,length(iters))
     visible.data<-data[i:(sequence_size+(i-1)),]
     data.x<-append(data.x,list(as.data.frame(visible.data[1:input_size,])))
     data.y<-append(data.y,list(as.data.frame(visible.data[(input_size+1):nrow(visible.data),])))
 
   }
+  parallel::stopCluster(cl)
   list(data.x,data.y)
 }
