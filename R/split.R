364ef4816e4b538ce053363155ee88cda5b191dd
lucas <lucas.ma8338@gmail.com>
2022-08-02 08:02:20 -0300

    changed split to test speed improvement

--- R/split.R
+++ R/split.R
@@ -17,12 +17,14 @@ split.x_y_timeseries_split<- function(data,input_size,output_size,n_jobs=-1){
   data.x<-list()
   data.y<-list()
   iters<- 1:((nrow(data)-sequence_size)+1)
-  foreach::foreach(i = iters)%dopar%{
+  process<- function(i){
     svMisc::progress(i,length(iters))
     visible.data<-data[i:(sequence_size+(i-1)),]
     data.x<-append(data.x,list(as.data.frame(visible.data[1:input_size,])))
     data.y<-append(data.y,list(as.data.frame(visible.data[(input_size+1):nrow(visible.data),])))
-
+  }
+  for (i in iters){
+    process(i)
   }
   if (n_jobs!=0){
     parallel::stopCluster(cl)
