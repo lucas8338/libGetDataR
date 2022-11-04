1e28e59ae04b7ee072a36516ddd8ccac3c0a7b33
lucas <lucas.ma8338@gmail.com>
2022-11-03 21:24:58 -0300

    1- remake of the function scaler.min_max, it is now a S3 class instead a S4

--- R/scaler.R
+++ R/scaler.R
@@ -3,32 +3,48 @@
 library(dplyr)
 library(glue)
 
-setClass("scaler.min_max", representation(stat = "data.frame"))
-setMethod("predict", "scaler.min_max",
-          function(object, data){
-            for (column in colnames(object@stat)){if (!column %in% colnames(data)){stop(glue::glue("column: {column} was there in the fitted data, but dont there in this data, is missing a column in the data"))}}
-            for (column in colnames(object@stat)){
-              min <- object@stat['min', column]
-              max <- object@stat['max', column]
-              data[[column]] <- lapply(data[, column], FUN = function(x){(x - min) / (max - min)}) %>% unlist()
-            }
-            data
-          }
-)
-
 #' @title scaler.min_max: a min_max scaler
 #' @param data: is the data to be applied the transformation
 #' @param column: a vector/list wtth the names of columns to apply the transformation
 #' @return: return a instance of class 'scaler.min_max' to use the 'predict' method
 #' @export
-scaler.min_max <- function(data, columns){
-  stat <- data.frame(row.names = c('min', 'max'))
-  for (column in columns){
-    stat['min', column] <- min(data[, column])
-    stat['max', column] <- max(data[, column])
-    if (stat['min', column]%>%is.numeric()==FALSE | stat['max', column]%>%is.numeric()==FALSE){
-      stop(glue("some value of the column: {column} is not numeric"))
+scaler.min_max <- function(data, columns='all'){
+  if ( columns=='all' ){columns<- colnames(data)}
+
+  for ( column in columns ){
+    if ( is.numeric(data[[column]])==FALSE ){
+      stop(glue::glue("the column: {column} is not numeric. All columns need to be numeric and have'nt NA"))
     }
   }
-  new("scaler.min_max", stat = stat)
+
+  stopifnot("there NA in the data"=any(is.na(data[,columns]))==FALSE)
+
+  stat <- data.frame(matrix(nrow = 2,ncol = length(columns)))
+  rownames(stat)<- c('min','max')
+  colnames(stat)<- columns
+  for (column in columns){
+    stat[c('min','max'), column] <- c( min(data[[column]]) , max(data[[column]]) )
+  }
+
+  result<- list(stat=stat,columns=columns)
+
+  class(result)<- 'scaler.min_max'
+
+  result
+}
+
+#' @exportS3Method
+predict.scaler.min_max<- function(fitted,data){
+  stat<- fitted[['stat']]
+  columns<- fitted[['columns']]
+
+  stopifnot("there columns are different from the fitted"=any(colnames(data) %in% columns==FALSE)==FALSE)
+
+  for ( column in columns ){
+    min<- stat['min',column]
+    max<- stat['max',column]
+    data[[column]]<- ( data[[column]] - min )/( max - min )
+  }
+
+  data
 }
